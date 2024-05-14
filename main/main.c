719c2f788d408bf21e4858536d7ae1988a40d26d
leandroCv-25 <leandrocamposvargas@gmail.com>
2024-05-14 17:59:58 -0300

    nvs initializing

--- main/main.c
+++ main/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include <nvs_flash.h>
+
 void app_main(void)
 {
+    /* Initialize NVS partition */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        /* NVS partition was truncated
+         * and needs to be erased */
+        ESP_ERROR_CHECK(nvs_flash_erase());
+
+        /* Retry nvs_flash_init */
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
 
 }
