9292ad138bfb1e18e83996827d30438e2c11b34a
leandroCv-25 <leandrocamposvargas@gmail.com>
2024-05-22 00:40:49 -0300

    Test

--- main/main.c
+++ main/main.c
@@ -52,7 +52,7 @@ void app_main(void)
     rmt_rx_channel_config_t rx_channel_cfg = {
         .clk_src = RMT_CLK_SRC_DEFAULT,
         .resolution_hz = 1000000, // 1MHz resolution, 1 tick = 1us
-        .mem_block_symbols = 64,  // amount of RMT symbols that the channel can store at a time
+        .mem_block_symbols = 200,  // amount of RMT symbols that the channel can store at a time
         .gpio_num = 19,
     };
 
@@ -77,7 +77,7 @@ void app_main(void)
     ESP_ERROR_CHECK(rmt_enable(rx_channel));
 
     // save the received RMT symbols
-    rmt_symbol_word_t raw_symbols[64]; // 64 symbols should be sufficient for a standard NEC frame
+    rmt_symbol_word_t raw_symbols[200]; // 200 symbols should be sufficient for a standard NEC frame
     rmt_rx_done_event_data_t rx_data;
     // ready to receive
     ESP_ERROR_CHECK(rmt_receive(rx_channel, raw_symbols, sizeof(raw_symbols), &receive_config));
