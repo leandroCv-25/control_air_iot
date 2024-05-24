49f147c2d1057c89d51eea14a525b8d71b7287d2
leandroCv-25 <leandrocamposvargas@gmail.com>
2024-05-24 15:35:19 -0300

    Test TX

--- main/main.c
+++ main/main.c
@@ -7,32 +7,243 @@
 #include "esp_log.h"
 #include <nvs_flash.h>
 
+#include "rx_control.h"
 #include "driver/rmt_tx.h"
-#include "driver/rmt_rx.h"
 
 static const char *TAG = "main";
-/**
- * @brief Decode RMT symbols into NEC scan code and print the result
- */
-static void example_parse_nec_frame(rmt_symbol_word_t *rmt_nec_symbols, size_t symbol_num)
-{
-    printf("Frame start---\r\n");
-    for (size_t i = 0; i < symbol_num; i++)
-    {
-        printf("{%d:%d},{%d:%d}\r\n", rmt_nec_symbols[i].level0, rmt_nec_symbols[i].duration0,
-               rmt_nec_symbols[i].level1, rmt_nec_symbols[i].duration1);
-    }
-    printf("---Frame end: ");
-}
 
-static bool rmt_rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata, void *user_data)
-{
-    BaseType_t high_task_wakeup = pdFALSE;
-    QueueHandle_t receive_queue = (QueueHandle_t)user_data;
-    // send the received RMT symbols to the parser task
-    xQueueSendFromISR(receive_queue, edata, &high_task_wakeup);
-    return high_task_wakeup == pdTRUE;
-}
+// Recorded command of each value for ON.
+uint32_t powerOff[] =
+    {
+        3204,
+        1475,
+        614,
+        972,
+        615,
+        972,
+        615,
+        205,
+        615,
+        204,
+        617,
+        203,
+        615,
+        971,
+        615,
+        205,
+        615,
+        204,
+        615,
+        972,
+        617,
+        969,
+        615,
+        205,
+        615,
+        971,
+        615,
+        205,
+        616,
+        204,
+        615,
+        971,
+        615,
+        972,
+        615,
+        204,
+        617,
+        970,
+        617,
+        969,
+        616,
+        204,
+        615,
+        205,
+        615,
+        971,
+        616,
+        204,
+        615,
+        204,
+        616,
+        204,
+        615,
+        971,
+        616,
+        204,
+        615,
+        204,
+        616,
+        204,
+        616,
+        203,
+        616,
+        204,
+        617,
+        202,
+        617,
+        202,
+        617,
+        203,
+        616,
+        203,
+        617,
+        203,
+        617,
+        202,
+        617,
+        203,
+        616,
+        203,
+        617,
+        203,
+        616,
+        203,
+        617,
+        203,
+        617,
+        202,
+        617,
+        203,
+        617,
+        202,
+        617,
+        203,
+        617,
+        969,
+        618,
+        202,
+        618,
+        201,
+        620,
+        200,
+        617,
+        202,
+        618,
+        202,
+        618,
+        201,
+        618,
+        201,
+        618,
+        202,
+        618,
+        201,
+        620,
+        200,
+        619,
+        200,
+        618,
+        202,
+        618,
+        201,
+        618,
+        201,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        619,
+        200,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        620,
+        199,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        619,
+        200,
+        619,
+        201,
+        618,
+        201,
+        621,
+        199,
+        618,
+        201,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        618,
+        201,
+        619,
+        201,
+        619,
+        200,
+        620,
+        199,
+        619,
+        201,
+        619,
+        200,
+        620,
+        200,
+        619,
+        200,
+        619,
+        201,
+        619,
+        200,
+        619,
+        201,
+        619,
+        200,
+        619,
+        201,
+        619,
+        200,
+        619,
+        201,
+        619,
+        200,
+        620,
+        200,
+        620,
+        199,
+        620,
+        967,
+        619,
+        200,
+        620,
+        967,
+        620,
+        200,
+        619,
+        200,
+        619,
+        968,
+        619,
+        967,
+        620,
+        200,
+        619,
+        0,
+};
 
 void app_main(void)
 {
@@ -48,48 +259,39 @@ void app_main(void)
         ESP_ERROR_CHECK(nvs_flash_init());
     }
 
-    ESP_LOGI(TAG, "create RMT RX channel");
-    rmt_rx_channel_config_t rx_channel_cfg = {
+    ESP_LOGI(TAG, "create RMT TX channel");
+    rmt_tx_channel_config_t tx_channel_cfg = {
         .clk_src = RMT_CLK_SRC_DEFAULT,
-        .resolution_hz = 1000000, // 1MHz resolution, 1 tick = 1us
-        .mem_block_symbols = 200,  // amount of RMT symbols that the channel can store at a time
-        .gpio_num = 19,
+        .resolution_hz = 10000000,
+        .mem_block_symbols = 64, // amount of RMT symbols that the channel can store at a time
+        .trans_queue_depth = 4,  // number of transactions that allowed to pending in the background, this example won't queue multiple transactions, so queue depth > 1 is sufficient
+        .gpio_num = 18,
+    };
+    rmt_channel_handle_t tx_channel = NULL;
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_channel_cfg, &tx_channel));
+
+    ESP_LOGI(TAG, "modulate carrier to TX channel");
+    rmt_carrier_config_t carrier_cfg = {
+        .duty_cycle = 0.33,
+        .frequency_hz = 38000, // 38KHz
+    };
+    ESP_ERROR_CHECK(rmt_apply_carrier(tx_channel, &carrier_cfg));
+
+    rmt_transmit_config_t transmit_config = {
+        .loop_count = 0, // no loop
     };
 
-    rmt_channel_handle_t rx_channel = NULL;
-    ESP_ERROR_CHECK(rmt_new_rx_channel(&rx_channel_cfg, &rx_channel));
+    rmt_encoder_handle_t encoder = NULL;
 
-    ESP_LOGI(TAG, "register RX done callback");
-    QueueHandle_t receive_queue = xQueueCreate(1, sizeof(rmt_rx_done_event_data_t));
-    assert(receive_queue);
-    rmt_rx_event_callbacks_t cbs = {
-        .on_recv_done = rmt_rx_done_callback,
-    };
-    ESP_ERROR_CHECK(rmt_rx_register_event_callbacks(rx_channel, &cbs, receive_queue));
+    ESP_LOGI(TAG, "enable RMT TX channel");
+    ESP_ERROR_CHECK(rmt_enable(tx_channel));
 
-    // the following timing requirement is based on NEC protocol
-    rmt_receive_config_t receive_config = {
-        .signal_range_min_ns = 1250,     // the shortest duration for NEC signal is 560us, 1250ns < 560us, valid signal won't be treated as noise
-        .signal_range_max_ns = 12000000, // the longest duration for NEC signal is 9000us, 12000000ns > 9000us, the receive won't stop early
-    };
-
-    ESP_LOGI(TAG, "enable RMT RX channel");
-    ESP_ERROR_CHECK(rmt_enable(rx_channel));
-
-    // save the received RMT symbols
-    rmt_symbol_word_t raw_symbols[200]; // 200 symbols should be sufficient for a standard NEC frame
-    rmt_rx_done_event_data_t rx_data;
-    // ready to receive
-    ESP_ERROR_CHECK(rmt_receive(rx_channel, raw_symbols, sizeof(raw_symbols), &receive_config));
-    while (1)
+    while (true)
     {
-        // wait for RX done signal
-        if (xQueueReceive(receive_queue, &rx_data, pdMS_TO_TICKS(1000)) == pdPASS)
-        {
-            // parse the receive symbols and print the result
-            example_parse_nec_frame(rx_data.received_symbols, rx_data.num_symbols);
-            // start receive again
-            ESP_ERROR_CHECK(rmt_receive(rx_channel, raw_symbols, sizeof(raw_symbols), &receive_config));
-        }
+        ESP_ERROR_CHECK(rmt_transmit(tx_channel, encoder, powerOff, sizeof(powerOff), &transmit_config));
+        printf("%d", sizeof(powerOff) / sizeof(u_int32_t));
+        vTaskDelay(pdMS_TO_TICKS(1500000));
     }
+
+    // rx_control_config(19, 1000000, 228, 1250, 64000000);
 }
